7efccf77fdc8f4e57716d947bbcef44dd9393b5b
Emma Amalie Pedersen <eapeders@stud.ntnu.no>
2022-10-21 11:07:45 +0200

    #5 component: Created navbar

--- meshgames/frontend/src/Components/Header/Header.tsx
+++ meshgames/frontend/src/Components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 function Header() {
   return (
     <div className="header">
-       <h1>MeshGames</h1>
+      <FontAwesomeIcon icon={faBars} />
+      <h1>MeshGames</h1>
     </div>
   );
 }
