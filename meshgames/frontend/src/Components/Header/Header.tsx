3da744ad78f13ade4c2f613bbad8e3ad02ba1413
Hang Celin Le <hcle@stud.ntnu.no>
2022-10-22 15:31:16 +0200

    #5 frontend: create sidebar

--- meshgames/frontend/src/Components/Header/Header.tsx
+++ meshgames/frontend/src/Components/Header/Header.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import './Header.css';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 
 
 function Header() {
+
+  const [showNavbar, setShowNavbar] = useState(false);
+
+const ToggleSidebar = () => {
+    const [isOpen, setIsopen] = useState(false);
+
+    const ToggleSidebar = () => {
+        isOpen === true ? setIsopen(false) : setIsopen(true);
+    }
+}
+
   return (
     <div className="header">
-      <FontAwesomeIcon icon={faBars} />
       <h1>MeshGames</h1>
+
+      <input type="checkbox" className="openSidebarMenu" id="openSidebarMenu"/>
+        <label htmlFor="openSidebarMenu" className="sidebarIconToggle">
+          <div className="spinner diagonal part-1"></div>
+          <div className="spinner horizontal"></div>
+          <div className="spinner diagonal part-2"></div>
+        </label>
+
+      <div id="sidebarMenu">
+        <ul className="sidebarMenuInner">
+          <li>Home</li>
+          <li>Your favorites</li>
+          <li>Explore</li>
+          <li>Charts</li>
+        </ul>
+      </div>
     </div>
   );
 }
