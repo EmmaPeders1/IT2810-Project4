713d11843092e5813ab6ebc1691807859445860a
Hang Celin Le <hcle@stud.ntnu.no>
2022-10-22 20:46:48 +0200

    #9 frontend: adjusted sidebar

--- meshgames/frontend/src/App.tsx
+++ meshgames/frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Favorites from './Pages/Favorites';
+import Charts from './Pages/Charts';
+import About from './Pages/About';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/charts" element={<Charts />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
