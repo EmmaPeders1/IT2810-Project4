69415aeecd27b4618e79fc9f8f3150e6cf1059b8
Emma Amalie Pedersen <eapeders@stud.ntnu.no>
2022-10-24 12:28:19 +0200

    #7 component: created card-component

--- meshgames/frontend/src/Pages/Home.tsx
+++ meshgames/frontend/src/Pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import GameCard from '../Components/GameCard/GameCard';
 
 export default function Home(){
 
 return(
   <div className="home">
     <h1>Home</h1>
+    <GameCard />
   </div>
 
   );
