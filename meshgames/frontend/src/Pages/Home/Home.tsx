3153321ee8eef2501dadbd9001aaa8e46f3278c9
Emma Amalie Pedersen <eapeders@stud.ntnu.no>
2022-10-30 14:53:41 +0100

    #27 frontend: Added function to check for data length

--- meshgames/frontend/src/Pages/Home/Home.tsx
+++ meshgames/frontend/src/Pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import GameCard from '../../Components/GameCard/GameCard';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown, faSearch } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from 'react';
 import Button from '../../Components/Button/Button';
 import './Home.css';
@@ -46,11 +46,11 @@ export default function Home() {
 
   const { loading, error, data, fetchMore } = useQuery(getGameDataForCards, { variables: info });
 
-
   function handleInput(event: React.MouseEvent<HTMLButtonElement>) {
     let input = document.getElementById("search-game") as HTMLInputElement;
     setInput(input.value);
     setLimit(8);
+    checkDataLength();
   }
 
   function handleFilter(platformInput: string, publisherInput: string, genreInput: string) {
@@ -58,10 +58,30 @@ export default function Home() {
     setPublisherName(publisherInput);
     setGenreName(genreInput);
     setLimit(8);
+    checkDataLength();
   }
 
+  function checkDataLength(){
+    let loadMore = document.getElementById("loadButton-container") as HTMLDivElement;
+    let zeroMessage = document.getElementById("zero-message") as HTMLParagraphElement;
+    if(loadMore != null && zeroMessage != null){
+      if(data.games.length === 0){
+        loadMore.style.display = "none";
+        zeroMessage.style.display = "inline";
+        console.log("length is zero");
+      }
+      else if(data.games.length < 8){
+        loadMore.style.display = "none";
+      }
+      else{
+        loadMore.style.display = "inline";
+        zeroMessage.style.display = "none";
+        console.log(data.games.length);
+      }
+    }
+  }
 
-  //known bug: the first time "load more is pressed", nothing happens. The following times, the expected behaviour happens.
+  //known bug: the first time "load more" is pressed, nothing happens. The following times, the expected behaviour happens.
   //we believe this to be a problem regarding the cache. On the web, many people have experienced something similar, but we could'nt find a solution
   function handleLoadMore(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
@@ -97,6 +117,7 @@ export default function Home() {
         <FilterBox handleFilter={handleFilter} />
       </div>
       <div className='gamecard-container'>
+        <p id="zero-message">There are no games that match your search</p>
         {data.games.map((cardData: CardDataProps) =>
           <GameCard
             key={cardData.gameId}
@@ -106,12 +127,14 @@ export default function Home() {
             platform={cardData.platform.platformId}
             genre={cardData.genre.genreId} />)}
       </div>
-      <div className="loadButton-container">
-
+      <div className="loadButton-container" id="loadButton-container">
         <Button
           className="load-button"
-          onClick={handleLoadMore}>
-          <p>"Load More.."</p></Button>
+          id="load-button"
+          label='LOAD MORE'
+          icon={faArrowDown}
+          onClick={handleLoadMore}
+        />
       </div>
     </div>
   );
