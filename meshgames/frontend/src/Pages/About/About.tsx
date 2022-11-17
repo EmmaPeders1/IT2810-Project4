af4993b7f1ea2b5f40e299be9695ac7eaf7cdfda
Emma Amalie Pedersen <eapeders@stud.ntnu.no>
2022-11-17 18:00:13 +0100

    frontend: changed about-page for this project

--- meshgames/frontend/src/Pages/About/About.tsx
+++ meshgames/frontend/src/Pages/About/About.tsx
@@ -8,7 +8,11 @@ return(
   <div className="about">
     <h1>About the application</h1>
     <p>
-      MeshGames is a web application based on data from the data set at Kaggle.com.
+      eGAMES strives for further improvements is the terms of accessability.
+
+      <br />
+      The application is based on the project 'MeshGames'.
+      MeshGames was based on data from the data set at Kaggle.com.
       The data consists of 16 598 games with their ranking, title, publisher, release year, genre, platform and sales in different world areas.
       Multiple games with the same title but on different platforms will be seen as different items.
       <br /> <br />
